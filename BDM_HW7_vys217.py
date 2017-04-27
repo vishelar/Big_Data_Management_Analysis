d689ca803bc7ead7823f0e92af188fd356b2d220
Vishwajeet Shelar <vishelar@gmail.com>
2017-04-27 03:15:13 -0400

    Add files via upload

--- BDM_HW7_vys217.py
+++ BDM_HW7_vys217.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext as sc
 from datetime import date, datetime
 from datetime import timedelta
-import pyproj
+from geopy.distance import vincenty
 
 def f(splitIndex, iterator): 
     if splitIndex == 0:
@@ -13,19 +13,6 @@ def f(splitIndex, iterator):
             a = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S+%f")
             yield a, row[0]
             
-def cal_dist(origin, destination):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    
-    proj = pyproj.Proj(init='EPSG:2263', preserve_units = True)
-    station = proj(lon1, lat1)
-    station = map(lambda x: x/5280, station)
-    
-    ys = proj(lon2, lat2)
-    ys = map(lambda x: x/5280, ys)
-    
-    return np.sqrt((ys[0] - station[0])**2 + (ys[1] - station[1])**2)
-
 def filter_recs_dist(splitIndex, iterator):
     if splitIndex == 0:
         iterator.next()
@@ -33,7 +20,7 @@ def filter_recs_dist(splitIndex, iterator):
     reader = csv.reader(iterator)
     for row in reader:
         if (row[4] != 'NULL') & (row[5] != 'NULL'):
-            if cal_dist((40.73901691,-74.00263761),(float(row[4]), float(row[5]))) <= 0.25:
+            if (vincenty((40.73901691,-74.00263761), (float(row[4]), float(row[5]))).miles) <= 0.25:
                 a = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
                 b = a + timedelta(seconds = 600)
                 yield a,b
