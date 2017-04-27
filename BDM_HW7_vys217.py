6cc998ad31472f6dec05563f060aab6bdeb4a707
Vishwajeet Shelar <vishelar@gmail.com>
2017-04-27 10:04:37 -0400

    Add files via upload

--- BDM_HW7_vys217.py
+++ BDM_HW7_vys217.py
@@ -5,17 +5,17 @@ from datetime import timedelta
 from geopy.distance import vincenty
 from pyspark.sql import HiveContext
 
-def f(splitIndex, iterator): 
+def citi_reader(splitIndex, iterator): 
     if splitIndex == 0:
         iterator.next()
     import csv
     reader = csv.reader(iterator)
     for row in reader:
         if (row[3][:10] == '2015-02-01') & (row[6] == 'Greenwich Ave & 8 Ave'):
-            a = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S+%f")
-            yield a, row[0]
+            start = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S+%f")
+            yield start, row[0]
             
-def filter_recs_dist(splitIndex, iterator):
+def filter_records_dist(splitIndex, iterator):
     if splitIndex == 0:
         iterator.next()
     import csv
@@ -23,22 +23,27 @@ def filter_recs_dist(splitIndex, iterator):
     for row in reader:
         if (row[4] != 'NULL') & (row[5] != 'NULL'):
             if (vincenty((40.73901691,-74.00263761), (float(row[4]), float(row[5]))).miles) <= 0.25:
-                a = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
-                b = a + timedelta(seconds = 600)
-                yield a,b
+                stop = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
+                extend = stop + timedelta(seconds = 600)
+                yield stop, extend
 
-sc = SparkContext()                
-spark = HiveContext(sc)
+def main(sc):
+    spark = HiveContext(sc)
+    
+    yellow = sc.textFile('/tmp/yellow.csv.gz').cache()
+    citibike = sc.textFile('/tmp/citibike.csv').cache()
 
-yellow = sc.textFile('/tmp/yellow.csv.gz').cache()
-citibike = sc.textFile('/tmp/citibike.csv').cache()
+    cb = citibike.mapPartitionsWithIndex(citi_reader)
+    yd = yellow.mapPartitionsWithIndex(filter_records_dist)
 
-cb = citibike.mapPartitionsWithIndex(f)
-yd = yellow.mapPartitionsWithIndex(filter_recs_dist)
+    cb_df = cb.toDF(['started', 'ride'])
+    yd_df = yd.toDF(['dropped', 'extended'])
 
-cb_df = cb.toDF(['started', 'ride'])
-yd_df = yd.toDF(['dropped', 'extended'])
+    joined_df = yd_df.join(cb_df).filter((yd_df.dropped < cb_df.started) & (yd_df.extended > cb_df.started))
 
-d = yd_df.join(cb_df).filter((yd_df.dropped < cb_df.started) & (yd_df.extended > cb_df.started))
+    print len(joined_df.toPandas()['ride'].unique())
 
-print len(d.toPandas()['ride'].unique())
+if __name__ == "__main__":
+    sc = SparkContext()
+    main(sc)
+
